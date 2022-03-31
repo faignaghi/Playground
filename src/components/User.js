5d20260831c3756f267879ab7ccccde19c323306
faignaghi <faignaghi@gmail.com>
2022-03-31 12:42:59 +0200

    interface with bootstrap added

--- src/components/User.js
+++ src/components/User.js
@@ -12,14 +12,17 @@ class User extends Component {
     // Destructing
     const { name, salary, department } = this.props;
     return (
-      <div>
-        <ul>
-          <li>
-            Isim : {name} <i className="fa-regular fa-trash-can"></i>
-          </li>
-          <li>Maas : {salary}</li>
-          <li>Departman : {department}</li>
-        </ul>
+      <div className="col-md-8 mb-4">
+        <div className="card">
+          <div className="card-header d-flex justify-content-between">
+            <h4 className="d-inline">{name}</h4>
+            <i className="far fa-trash-alt" style={{ cursor: "pointer" }}></i>
+          </div>
+          <div className="card-body">
+            <p className="card-text">Maas : {salary}</p>
+            <p className="card-text">Department : {department}</p>
+          </div>
+        </div>
       </div>
     );
   }
