eff03df0d8fade4e72f09599ee0ca9e936330bab
faignaghi <faignaghi@gmail.com>
2022-03-31 16:49:52 +0200

    connection between components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import User from "./components/User";
+import Users from "./components/Users";
 
 class App extends Component {
+  state = {
+    users: [
+      {
+        id: 1,
+        name: "John Smith",
+        salary: "5000",
+        department: "IT",
+      },
+      {
+        id: 2,
+        name: "Albert Dowson",
+        salary: "7000",
+        department: "IT-Problems",
+      },
+      {
+        id: 3,
+        name: "Faig Naghiyev",
+        salary: "15000",
+        department: "IT-Solutions",
+      },
+    ],
+  };
+
   render() {
     return (
       <div className="container">
         <Navbar title="User App" />
         <hr />
-        <User name="Faig Naghiyev" salary="25000" department="IT" />
-
-        <User name="Naghi Faig" salary="15000" department="IT-City" />
+        <Users users={this.state.users} />
       </div>
     );
   }
