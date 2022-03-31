bbe2b495a369d410c46dfc7ad4092db2f6362953
faignaghi <faignaghi@gmail.com>
2022-03-31 14:50:37 +0200

    react events

--- src/components/User.js
+++ src/components/User.js
@@ -12,6 +12,15 @@ class User extends Component {
     department: " Bilgi yok",
   };
 
+  constructor(props) {
+    super(props);
+    this.onClickEvent = this.onClickEvent.bind(this);
+  }
+
+  onClickEvent = (number, e) => {
+    console.log(number);
+  };
+
   // constructor(props) {
   //   super(props);
 
@@ -24,11 +33,14 @@ class User extends Component {
     // Destructing
     const { name, salary, department } = this.props;
     const { isVisible } = this.state;
+
     return (
       <div className="col-md-8 mb-4">
         <div className="card">
           <div className="card-header d-flex justify-content-between">
-            <h4 className="d-inline">{name}</h4>
+            <h4 className="d-inline" onClick={this.onClickEvent.bind(this, 34)}>
+              {name}
+            </h4>
             <i className="far fa-trash-alt" style={{ cursor: "pointer" }}></i>
           </div>
           {isVisible ? (
